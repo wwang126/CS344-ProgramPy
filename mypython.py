05f45a6654e45941276c78a5ec48e136b96ad197
wwang126 <wavelet.wang@gmail.com>
2017-11-22 16:30:56 -0800

    added random string generator for the file creators

--- mypython.py
+++ mypython.py
@@ -1,8 +1,35 @@
 import random
+import string
 
-#Open/Create files
+#Create random strings
+#get string 1
+f1str = ""
+for j in range(0,10):
+    i = random.randrange(97,123)
+    char = chr(i)
+    f1str += char
+print(f1str, end= "")
+#get string 2
+f2str = ""
+for j in range(0,10):
+    i = random.randrange(97,123)
+    char = chr(i)
+    f2str += char
+print(f2str)
+#get string 3
+f3str = ""
+for j in range(0,10):
+    i = random.randrange(97,123)
+    char = chr(i)
+    f3str += char
+print(f3str)
+
+
+#Open/Create file1
 f1 = open("file1.txt","w+")
+#Open/Create file1
 f2 = open("file2.txt","w+")
+#Open/Create file1
 f3 = open("file3.txt","w+")
 
 #get random values from 1 to 42 inclusive
