f868639f6d0c0de89641049271784e9147f4ebf8
wwang126 <wavelet.wang@gmail.com>
2017-11-22 16:35:59 -0800

    added writing to files and closing of files.

--- mypython.py
+++ mypython.py
@@ -8,6 +8,9 @@ for j in range(0,10):
     i = random.randrange(97,123)
     char = chr(i)
     f1str += char
+#Add new line character
+f1str += "\n"
+#Suppress print new line
 print(f1str, end= "")
 #get string 2
 f2str = ""
@@ -15,22 +18,40 @@ for j in range(0,10):
     i = random.randrange(97,123)
     char = chr(i)
     f2str += char
-print(f2str)
+#Add new line character
+f2str += "\n"
+#Suppress print new line
+print(f2str, end = "")
 #get string 3
 f3str = ""
 for j in range(0,10):
     i = random.randrange(97,123)
     char = chr(i)
     f3str += char
-print(f3str)
+#Add new line character
+f3str += "\n"
+#Suppress print new line
+print(f3str, end = "")
 
 
 #Open/Create file1
 f1 = open("file1.txt","w+")
-#Open/Create file1
+#Write into file1
+f1.write(f1str);
+#Close file1
+f1.close()
+#Open/Create file2
 f2 = open("file2.txt","w+")
-#Open/Create file1
+#Write into file1
+f2.write(f2str);
+#Close file1
+f2.close()
+#Open/Create file3
 f3 = open("file3.txt","w+")
+#Write into file1
+f3.write(f3str);
+#Close file1
+f3.close()
 
 #get random values from 1 to 42 inclusive
 x = random.randrange(1,43)
