edd018ccef77e099e95bea904204142f3ace5eb6
wwang126 <wavelet.wang@gmail.com>
2017-11-22 16:30:23 -0800

    added created files to git ignore. now creates files

--- mypython.py
+++ mypython.py
@@ -1,5 +1,10 @@
 import random
 
+#Open/Create files
+f1 = open("file1.txt","w+")
+f2 = open("file2.txt","w+")
+f3 = open("file3.txt","w+")
+
 #get random values from 1 to 42 inclusive
 x = random.randrange(1,43)
 y = random.randrange(1,43)
